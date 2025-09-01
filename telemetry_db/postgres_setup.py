aa696832e7decf4eacc4a597ce7ab0e3065d34c7
Travis Busen <busen@me.com>
2025-08-31 20:13:13 -0500

    removed -y

--- telemetry_db/postgres_setup.py
+++ telemetry_db/postgres_setup.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
         docker_key_commands = [
             "sudo apt-get update",
             "sudo apt-get install ca-certificates curl",
-            "sudo install -m 0755 -d /etc/apt/keyrings -y",
+            "sudo install -m 0755 -d /etc/apt/keyrings",
             "sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc",
             "sudo chmod a+r /etc/apt/keyrings/docker.asc",
         ]
