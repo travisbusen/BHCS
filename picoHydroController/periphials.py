70d8e856508d386823d4a1185072b7d6c5df1a0f
Travis Busen <busen@me.com>
2025-09-14 17:59:52 -0500

    abstracted classes for I2C sensors and temp, humiidty sensors

--- picoHydroController/periphials.py
+++ picoHydroController/periphials.py
@@ -1,6 +1,83 @@
 from machine import Pin, I2C
 import time
+from abc import ABC, abstractmethod
 
+
+
+class I2CInit:
+    def __init__(self, bus: int, serial_clock_pin: int, serial_data_pin: int, frequency: int):
+        self.bus = bus
+        self.serial_clock_pin = serial_clock_pin
+        self.serial_data_pin = serial_data_pin
+        self.frequency = frequency
+
+    def initiate_i2c(self):
+        self.i2c = I2C(self.bus, scl=Pin(self.serial_clock_pin), sda=Pin(self.serial_data_pin), freq=self.frequency)
+        return self.i2c
+    
+
+class I2CSensor(ABC):
+    def __init__(self, i2c: I2CInit, address: int):
+        self.i2c = i2c
+        self.address = address
+
+    @abstractmethod
+    def request_status(self, command: bytes) -> bool:
+        pass
+        
+    @abstractmethod
+    def request_measurement(self, command: bytes) -> bool:
+        pass
+        
+    @abstractmethod
+    def read_measurement(self, num_bytes: int) -> bytes:
+        pass
+
+
+class AHT21(I2CSensor):
+    def __init__(self, i2c: I2CInit, address: int):
+        super().__init__(i2c, address)
+        time.sleep_ms(110) # only needed on first boot, but harmless to run always
+
+    def request_status(self, command: bytes) -> bool:
+        try:
+            self.i2c.writeto(self.address, command)
+        except Exception as e:
+            return False
+        status = self.i2c.readfrom(self.address, 1)
+        if status == b'\x18': # bit 3 and 4 are 1 calibraded 
+            return True
+        else:
+            return False
+
+    def request_measurement(self, command: bytes) -> bool:
+        try:
+            self.i2c.writeto(self.address, command)
+        except Exception as e:
+            return False
+        time.sleep_ms(100) # wait for measurement to complete
+        return True
+
+    def read_measurement(self, num_bytes: int) -> bytes:
+        try:
+            data = self.i2c.readfrom(self.address, num_bytes)
+            if (data[0] >> 7) & 1 == 0: # check if bit 7 is 0, meaning data is ready
+                return data
+        except Exception as e:
+            return b''
+        
+    def get_humidity(self, data: bytes) -> float:
+        humidity_raw = (data[1] << 12) | (data[2] << 4) | (data[3] >> 4)
+        humidity = (humidity_raw / 2**20) * 100
+        return humidity
+    
+    def get_temperature(self, data: bytes) -> float:
+        temperature_raw = ((data[3] & 0x0F) << 16) | (data[4] << 8) | data[5]
+        temperature = ((temperature_raw / 2**20) * 200) - 50
+        return temperature
+
+
+    
 
 
 class DeviceController:
